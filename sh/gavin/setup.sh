577fff8e351c707174ead1e2ae8bde729051f21c
Gavin Engel <gavin@engel.com>
2016-01-05 22:33:59 -0500

    .

--- sh/gavin/setup.sh
+++ sh/gavin/setup.sh
@@ -8,6 +8,7 @@ git config --global url."https://".insteadOf git://; # solves github timeouts on
 ##sudo adduser -m gavin;
 
 ln -sf ~/decorate/sh/gavin/aliases/.bash_profile ~/;
+ln -sf ~/decorate/sh/gavin/aliases/.bashrc ~/;
 ln -sf ~/decorate/sh/gavin/aliases/.sh_common ~/;
 ln -sf ~/decorate/sh/gavin/aliases/.sh_custom ~/;
 ln -sf ~/decorate/sh/gavin/aliases/.bash_common ~/;
@@ -17,11 +18,8 @@ ln -sf ~/decorate/sh/gavin/aliases/.zsh_custom ~/;
 ## then, add to end of .zshrc and .bash_profile:
 # source ~/.sh_common
 
-
 npm config set init.author.name "Gavin Engel"
 npm config set init.author.email gavin@engel.com 
 npm config set init.author.url http://www.gavinengel.com/
 npm config set init.license MIT
 
-
-echo "Now, use the .bashrc included in decorate/sh/gavin/aliases.";
