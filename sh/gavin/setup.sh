b66a52642dffb7b2475c9ca9d536b1b8bfb2fbc1
Gavin Engel <gavin@engel.com>
2016-01-27 09:27:49 -0500

    .

--- sh/gavin/setup.sh
+++ sh/gavin/setup.sh
@@ -18,8 +18,13 @@ ln -sf ~/decorate/sh/gavin/aliases/.zsh_custom ~/;
 ln -sf ~/decorate/sh/gavin/aliases/.zshrc ~/;
 rm -rf ~/.gconf; ln -sf ~/decorate/sh/gavin/aliases/.gconf ~/;
 rm -rf ~/.config/terminator; ln -sf ~/decorate/sh/gavin/aliases/dot-config/terminator ~/.config/terminator;
-rm -rf ~/.config/midori; ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori ~/.config/midori;
+ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/bookmarks.db ~/.config/midori/;
+ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/speeddial ~/.config/midori/;
+ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/config ~/.config/midori/;
 
+gsettings set org.gnome.desktop.wm.preferences auto-raise true;
+gsettings set org.gnome.desktop.wm.preferences focus-mode sloppy;
+gsettings set org.gnome.desktop.wm.preferences auto-raise-delay 100;
 
 ## then, add to end of .bash_profile:
 # source ~/.sh_common
@@ -28,4 +33,5 @@ npm config set init.author.name "Gavin Engel"
 npm config set init.author.email gavin@engel.com 
 npm config set init.author.url http://www.gavinengel.com/
 npm config set init.license MIT
+npm set progress=false; # https://twitter.com/gavinjoyce/status/691773956144119808
 
