2fe828e3bf55a951d3f976866cf497bd570558e7
Gavin Engel <gavin@engel.com>
2016-03-03 17:15:54 -0500

    .

--- sh/debuntu/ufw.sh
+++ sh/debuntu/ufw.sh
@@ -3,7 +3,14 @@
 sudo ufw default deny incoming;
 sudo ufw default deny outgoing;
 sudo ufw allow ssh;
-sudo ufw allow http;
+sudo ufw allow http; 
 sudo ufw allow www;
 sudo ufw allow 2525/tcp; # mailtrap.io
+sudo ufw allow 9418;     # git
+sudo ufw allow out http;
+sudo ufw allow out 22;
+sudo ufw allow out 53;
+sudo ufw allow out 443;
+sudo ufw limit ssh/tcp;  #Limit connections to ssh/tcp to slowdown possible attacks
+#sudo ufw logging on
 sudo ufw enable;
