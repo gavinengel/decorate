370af31ea8369ad36cfe3b9865ed83f65267ed72
Gavin Engel <gavin@engel.com>
2016-04-30 18:05:29 -0400

    .

--- sh/debuntu/ufw.sh
+++ sh/debuntu/ufw.sh
@@ -11,6 +11,12 @@ sudo ufw allow out http;
 sudo ufw allow out 22;
 sudo ufw allow out 53;
 sudo ufw allow out 443;
+
+# dev ports, for various services
+# Harpjs = 9000
+#sudo ufw allow 9000:9000/tcp;
+#sudo ufw allow 9000:9000/udp;
+
 sudo ufw limit ssh/tcp;  #Limit connections to ssh/tcp to slowdown possible attacks
 #sudo ufw logging on
 sudo ufw enable;
