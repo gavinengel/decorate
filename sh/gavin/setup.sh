84a0ee926e1f2bf6c9675207cba6527713083daf
Gavin Engel <gavin@engel.com>
2016-05-20 12:10:05 -0400

    stuff

--- sh/gavin/setup.sh
+++ sh/gavin/setup.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-git config --global user.name "Gavin Engel";
-git config --global user.email gavin@engel.com;
-git config --global push.default simple;
-git config --global url."https://".insteadOf git://; # solves github timeouts on npm install
+#git config --global user.name "Gavin Engel";
+#git config --global user.email gavin@engel.com;
+#git config --global push.default simple;
+#git config --global url."https://".insteadOf git://; # solves github timeouts on npm install
 
 ##sudo adduser -m gavin;
 
 rm -rf ~/.gconf; ln -sf ~/decorate/sh/gavin/aliases/.gconf ~/;
 rm -rf ~/.config/terminator; ln -sf ~/decorate/sh/gavin/aliases/dot-config/terminator ~/.config/terminator;
-ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/bookmarks.db ~/.config/midori/;
-ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/speeddial ~/.config/midori/;
-ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/config ~/.config/midori/;
+#ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/bookmarks.db ~/.config/midori/;
+#ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/speeddial ~/.config/midori/;
+#ln -sf ~/decorate/sh/gavin/aliases/dot-config/midori/config ~/.config/midori/;
+rm -rf ~/.gitconfig; ln -sf ~/decorate/sh/gavin/aliases/.gitconfig ~/;
+
 
 gsettings set org.gnome.desktop.wm.preferences auto-raise false;
 gsettings set org.gnome.desktop.wm.preferences focus-mode sloppy;
